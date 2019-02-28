9aeb4fa35cad547e5489ad163a4abdd68b4a34b8
mikedoinggood <mikedoinggood@users.noreply.github.com>
2019-02-27 20:22:00 -0800

    Rails Unique Username Constraint
    - Added unique constraint migration for username

--- ruby-car-data/car_data/db/schema.rb
+++ ruby-car-data/car_data/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_102614) do
+ActiveRecord::Schema.define(version: 2019_02_28_012241) do
 
   create_table "cars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "year"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_01_09_102614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "trim_levels", "cars"
