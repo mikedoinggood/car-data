655650f32e57708d6d9d708f67227744fa526acb
mikedoinggood <mikedoinggood@users.noreply.github.com>
2020-09-22 20:05:54 -0700

    Revert "- Updated django api retrieval of cars to take into account the id.  This is to correct"
    This reverts commit 28427a0fa8d8dbe5caed4dec63ade3a828c83d8f.

--- python-car-data/django/car_data/api/views.py
+++ python-car-data/django/car_data/api/views.py
@@ -19,11 +19,11 @@ def cars_get(request):
     page_number = request.GET.get('page', '')
 
     if sort == "newest":
-        all_cars_list = Car.objects.order_by('-year', 'make', 'id').all()
+        all_cars_list = Car.objects.order_by('-year', 'make').all()
     elif sort == "oldest":
-        all_cars_list = Car.objects.order_by('year', 'make', 'id').all()
+        all_cars_list = Car.objects.order_by('year', 'make').all()
     else:
-        all_cars_list = Car.objects.order_by('make', 'id').all()
+        all_cars_list = Car.objects.order_by('make').all()
 
     paginator = Paginator(all_cars_list, page_size)
 
